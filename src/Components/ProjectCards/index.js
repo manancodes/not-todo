80d1f3b4ab7384be76f16d853c5422bc9830fd06
Manan <manansadana@Manans-MacBook-Air.local>
2022-05-31 22:37:57 +0530

    trying new colors

--- src/Components/ProjectCards/index.js
+++ src/Components/ProjectCards/index.js
@@ -4,11 +4,21 @@ import styles from "./index.module.css";
 export default function ProjectCards() {
   return (
     <div className={styles.container}>
-      <div className={styles.card}>This is a Project Card</div>
-      <div className={styles.card}>This is a Project Card</div>
-      <div className={styles.card}>This is a Project Card</div>
-      <div className={styles.card}>This is a Project Card</div>
-      <div className={styles.card}>This is a Project Card</div>
+      <div className={styles.card} style={{ background: "#5CE1E6" }}>
+        This is a Project Card
+      </div>
+      <div className={styles.card} style={{ background: "#FFE270" }}>
+        This is a Project Card
+      </div>
+      <div className={styles.card} style={{ background: "#FF7075" }}>
+        This is a Project Card
+      </div>
+      <div className={styles.card} style={{ background: "#9E86C1" }}>
+        This is a Project Card
+      </div>
+      <div className={styles.card} style={{ background: "#47E5BC" }}>
+        This is a Project Card
+      </div>
     </div>
   );
 }
